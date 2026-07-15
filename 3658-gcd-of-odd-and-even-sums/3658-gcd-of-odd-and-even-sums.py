7917a4cee19b3494d05f0ab17a2ddc6a6d46a46d
Harshit-Singh-Rao <harshitsrao1@gmail.com>
2026-07-15 20:08:34 +0530

    Time: 0 ms (100%), Space: 19.4 MB (21.29%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,12 +1,5 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        listodd=[]
-        listeven=[]
-        for x in range(1,2*n+1):
-            if x%2==0:
-                listeven.append(x)
-            elif x%2==1:    
-                listodd.append(x)
-        sumOdd=sum(listodd)
-        sumEven=sum(listeven)
+        sumOdd=n**2
+        sumEven=n*(n+1)
         return math.gcd(sumOdd,sumEven)
