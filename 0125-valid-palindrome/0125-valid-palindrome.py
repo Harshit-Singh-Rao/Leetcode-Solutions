a1b8fa6fec01110cf4c4cf726e8dd4b167bdaa0d
Harshit-Singh-Rao <harshitsrao1@gmail.com>
2026-08-29 21:08:26 +0530

    Time: 443 ms (5.51%), Space: 19.7 MB (53.19%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        for i in s:
+            if i not in ("abcdefghijklmnopqrstuvwxyz0123456789"):
+                s=s.replace(i,"")
+        if s==s[::-1]:
+            return True
+        else:
+            return False
